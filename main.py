ee7b8fb5678e8b5e66d22aa513e5b60f86d731fd
LotovIgor <ilotov@ngs.ru>
2020-09-18 14:59:54 +0700

    Скелет

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
+def point (n):
+    #Помещение собаки в город на рандомные координаты
+#return x,y
+    pass
+
+def  up (x, y, city):
+    pass
+
+def  down (x, y, city):
+    pass
+
+def  right (x, y, city):
+    pass
+
+def  left (x, y, city):
+    pass
+
+def walk (x, y, city):
+    #Шаг собаки
+    pass
+
+def tupik (x, y, city):
+    #Проверка всех направлений на то была ли там собака
+    pass
+def gran (x, y, city):
+    #проверка на границу города
+    #Заканчивается ли таблица с одной из сторон
+    pass
+def dog (n):
+    #Просчитывает одно путешествие собаки в городе
+    pass
+
+
+if __name__ == '__main__':
+#Множество повторений функции и вероятность
+  pass
+
+
 
